48e1ba82c18820b90007f24c6ae23f974964b484
Raed Nouredine <raedrnp10@gmail.com>
2022-12-13 12:25:39 -0400

    "adding smooth scroll to the anchors"

---
+++ script.js
@@ -0,0 +1,11 @@
+//selected all links in navbar to smooth its scroll
+const links = document.querySelectorAll("a[href^='#']").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: "smooth", block: "end"
+        });
+        
+    });
+});
+
